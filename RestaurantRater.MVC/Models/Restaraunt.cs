40b8ccf10a184e0700c2928acdc7ba59d172184b
Max Edwards <medwards2099@gmail.com>
2019-10-07 10:10:13 -0400

    Add project files.

---
+++ RestaurantRater.MVC/Models/Restaraunt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.MVC.Models
+{
+    public class Restauraunt
+    {
+        public int RestaurantId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+    }
+
+    public class RestaurantDbContext: DbContext
+    {
+        public DbSet<Restauraunt> Restaurants { get; set; }
+    }
+
+}
